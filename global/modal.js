085deb47d7268a12a19daaa07dacff7da37c948e
Your Name <you@example.com>
2022-04-15 16:18:37 +0200

    From begin

--- global/modal.js
+++ global/modal.js
@@ -1,1 +1,51 @@
 "use strict";
+/**
+ *
+ * @param {HTMLElement} element
+ * @returns {object}
+ */
+
+const modal = function (element) {
+  if (element == null) throw Error("Nema elementa za modal!");
+
+  let close = function () {
+    let list = element.querySelector(".body ul");
+
+    list.innerHTML = "";
+    element.classList.remove("show");
+  };
+
+  let backgroundClose = (function (e) {
+    let back = document.querySelector("div.modal-container");
+    back.addEventListener("click", function (e) {
+      close();
+    });
+  })();
+
+  let buttonClose = (function () {
+    let button = element.querySelector(".header button.close");
+
+    if (button) {
+      button.addEventListener("click", function (e) {
+        e.preventDefault();
+        close();
+      });
+    }
+  })();
+
+  return {
+    add: function (messages) {
+      let body = element.querySelector("div.body ul");
+      if (typeof messages !== "object") messages = [messages];
+
+      messages.forEach((message) => {
+        let li = document.createElement("li");
+        li.innerHTML = message;
+        body.appendChild(li);
+      });
+    },
+    show: function () {
+      element.classList.add("show");
+    },
+  };
+};
--- global/validator.js
+++ global/validator.js
@@ -3,40 +3,28 @@ const validator = {
   errors: [],
 
   rules: {
-    maxLenght: function (
-      value,
-      request = 10,
-      message = "Input must have max 10 caracters!"
-    ) {
+    maxLenght: function (value, request = 10, message = "Input must have max 10 caracters!") {
       if (value.length != request) {
         validator.errors.push(message);
         return false;
       }
       return true;
     },
-    minLenght: function (
-      value,
-      request = 3,
-      message = "Input must have min 3 caracters!"
-    ) {
+    minLenght: function (value, request = 3, message = "Input must have min 3 caracters!") {
       if (value.length != request) {
         validator.errors.push(message);
         return false;
       }
       return true;
     },
-    empty: function (value, message = "Input is required") {
+    empty: function (value, message = value + "is required!") {
       if (value == "") {
         validator.errors.push(message);
         return false;
       }
       return true;
     },
-    match: function (
-      value1,
-      value2,
-      message = value1 + "must be the same like " + value2
-    ) {
+    match: function (value1, value2, message = value1 + "must be the same like " + value2) {
       if (value1.value !== value2.value) {
         validator.errors.push(message);
         return false;
@@ -48,6 +36,38 @@ const validator = {
         validator.errors.push(message);
         return false;
       }
+      return true;
+    },
+  },
+
+  inputs: {
+    email: function (email) {
+      validator.rules.empty(email, "Email is required !");
+      validator.rules.includes(email, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Email isn't in regular format : email@example.xxx !");
+    },
+
+    password: function (password, currentPassword) {
+      validator.rules.empty(password);
+      validator.rules.includes(password, /[A-Za-z]/, "Password must have a letter from A-z!");
+      validator.rules.includes(password, /\d/, "Password must have number!");
+      validator.rules.minLenght(password, 4, "Password must have min 4 caracters!");
+      validator.rules.maxLenght(password, 8, "Password must have max 8 caracters!");
+      if (currentPassword.value != "") {
+        validator.rules.match(password, currentPassword);
+        return false;
+      }
+      return true;
+    },
+
+    name: function (value) {
+      validator.rules.minLenght(value);
+      validator.rules.maxLenght(value, 15, "Name must have max 15 caracters!");
+      return true;
+    },
+
+    phone: function (value) {
+      validator.rules.includes(value, /^(\+)+[\d]{1,14}$/, "Phone number must be in format: +...!");
+      validator.rules.maxLenght(value, 15, "Phone must have max 15 caracters!");
       return true;
     },
   },
