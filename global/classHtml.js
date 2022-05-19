8604ab9cf7685242630874931d65cef12cb03291
Your Name <you@example.com>
2022-05-19 15:56:15 +0200

    19.05.22

--- global/classHtml.js
+++ global/classHtml.js
@@ -1,26 +1,49 @@
+"use strict";
 /**
  * @param {HTMLElement}element
  * @returns {object}
  */
 
 class Html {
-  constructor(element, inputData, numberElement) {
+  constructor(element, inputData = [], numberElement) {
     this.element = element;
     this.inputsData = inputData;
     this.numberElement = numberElement;
   }
 
-  row(type) {
-    let row = document.createElement(`${type}`);
-    this.wraper();
+  row(type, input = this.inputsData, inputCreate = false) {
+    let row = document.createElement("tr");
 
-    for (let i = 0; i < this.inputsData.length; i++) {
-      const element = this.inputsData[i];
-    }
     if (type === "td") {
       this.tbody().appendChild(row);
     } else if (type === "th") {
       this.thead().appendChild(row);
+    }
+
+    for (let i = 0; i < input.length; i++) {
+      const element = input[i];
+      let column = document.createElement(`${type}`);
+
+      // input[0] = this.createButton("add");
+      // row.appendChild(input[0]);
+      if (inputCreate) {
+        let createdInput = document.createElement("input");
+        // inputCreate.setAttribute("placeholder", element);
+
+        createdInput.value = element;
+        // inputCreate.appendChild(this.createButton());
+
+        column.appendChild(createdInput);
+      } else {
+        column.innerHTML = element;
+      }
+
+      //   if (inputCreate) {
+      //     this.createInput(element, column);
+      //   } else {
+      //     column.innerHTML = element;
+      //   }
+      row.append(column);
     }
 
     return row;
@@ -37,13 +60,29 @@ class Html {
 
   thead() {
     let thead = document.createElement("thead");
+
     this.element.appendChild(thead);
     return thead;
   }
   tbody() {
     let tbody = document.createElement("tbody");
+
     this.element.appendChild(tbody);
     return tbody;
+  }
+  createButton(innerButtonText) {
+    let button = document.createElement("button");
+    button.classList.add(`${innerButtonText}`);
+    button.innerText = `${innerButtonText}`;
+
+    return button;
+  }
+
+  createInput(element, column) {
+    let createdInput = document.createElement("input");
+    createdInput.value = element;
+
+    column.appendChild(createdInput);
   }
 }
 
@@ -52,6 +91,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   let tabelaHtml = new Html(exampleElement);
   tabelaHtml.wraper();
-  tabelaHtml.row("th");
-  tabelaHtml.row("td");
+  tabelaHtml.row("th", ["Action", "Name", "Adress", "ID", "Phone"]);
+  tabelaHtml.row("td", [1, 23, 22, 21], true);
+  // console.log(row1.querySelector("td input"));
+  //let selec = row1.querySelector("td input");
+  // tabelaHtml.row("td", [tabelaHtml.createButton("Add", selec), [23, 22, 21, 50]], true);
+
+  //let row1 = tabelaHtml.row("td", [23, 22, 21, 50], true);
+
+  //let dataIN = row1.querySelectorAll("td");
+  //console.log(dataIN);
 });
